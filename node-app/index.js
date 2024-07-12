6b6a2e282b30698a70b5465931fe8bc7631293d2
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-12 11:59:48 +0530

    ES6 modules - part - 1

--- node-app/index.js
+++ node-app/index.js
@@ -1,1 +1,25 @@
+/* 
+import * as calc from './calculator.js'
+console.log(calc) // module object with references to all the entities that are exported
+console.log(calc.add(100,200))
+console.log(calc.subtract(100,200)) 
+*/
+
+/* 
+import * as calc from './calculator.js'
+console.log(calc.add(10,20))
+console.log(calc.add(10,200)) 
+*/
+
+/* 
+import * as calc from "./calculator.js";
+// const add = calc.add;
+const { add } = calc; 
+*/
+
+import { add } from "./calculator.js";
+console.log(add(10, 20));
+console.log(add(10, 200));
+
+
 console.log('Welcome to Node.js!')
