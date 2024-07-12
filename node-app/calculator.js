561a0a42e0e3830479002fa754b85f78fcdb22ad
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-12 12:09:31 +0530

    es6 modules completed

--- node-app/calculator.js
+++ node-app/calculator.js
@@ -6,3 +6,10 @@ export function add(x,y){
 export function subtract(x,y){
     return x - y;
 }
+
+function sayHi(){
+    console.log('Hi there!')
+}
+
+// default export
+export default sayHi;
--- node-app/index.js
+++ node-app/index.js
@@ -17,9 +17,27 @@ import * as calc from "./calculator.js";
 const { add } = calc; 
 */
 
+/* 
 import { add } from "./calculator.js";
 console.log(add(10, 20));
 console.log(add(10, 200));
+*/
 
+// importing the default exported entity
+/* 
+import sayHi from "./calculator.js";
+sayHi() 
+*/
+
+/* 
+import hi from './calculator.js'
+hi() 
+*/
+
+// combining exports & 'default exports'
+import sayHi, {add, subtract} from './calculator.js'
+sayHi()
+console.log(add(100,200))
+console.log(subtract(100,200))
 
 console.log('Welcome to Node.js!')
